0aea132e090dbe25b08bcacf96fcd90408cddb2f
auaicn <crorowslowpp@gmail.com>
2020-03-27 18:47:55 +0900

    Update prac_syscall.c

--- xv6-public/prac_syscall.c
+++ xv6-public/prac_syscall.c
@@ -8,3 +8,17 @@ printk_str(char *str)
     cprintf("%s\n",str);
     return 0xABCDABCD;
 }
+
+int
+sys_myfunction(void)
+{
+    char *str;
+    
+    if(argstr(0,&str)<0)
+    {
+        return -1;
+    }
+    
+    return printk_str(str);
+    
+}
