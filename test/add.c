77d79d3855aa76688674065ac9c07b79346cbfc2
auaicn <crorowslowpp@gmail.com>
2020-04-24 22:05:10 +0900

    adding test code

---
+++ test/add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int
+main(int argc,const char* argv[])
+{
+	int a=1;
+	int b=2;
+	int c=a+b;
+	return 0;
+}
---
+++ test/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int 
+main (int argc, const char* argv[])
+{
+	printf("HELLO WORLD!\n");
+	return 0;
+}
+
