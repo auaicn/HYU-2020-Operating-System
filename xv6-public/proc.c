186bbc686798165505fef55955b42b0c5bfc60f9
2016024811 <crorowslowpp@gmail.com>
2020-04-25 17:23:59 +0900

    testing boosting

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -420,6 +420,8 @@ boost (void)
   total_tick = 0;
 }
 
+// to save
+/*
 void
 scheduler(void)
 {
@@ -433,6 +435,7 @@ scheduler(void)
 
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
+
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE)
         continue;
@@ -454,6 +457,54 @@ scheduler(void)
     release(&ptable.lock);
 
   }
+
+}
+*/
+
+void
+scheduler(void)
+{
+  struct proc *p;
+  struct cpu *c = mycpu();
+  c->proc = 0;
+
+  struct proc mlfq;
+  mlfq.share = 100;
+  mlfq.pass = 0;
+  stable.proc[0] = &mlfq;
+  
+  for(;;){
+    // Enable interrupts on this processor.
+    sti();
+
+    // Loop over process table looking for process to run.
+    acquire(&ptable.lock);
+
+    if(!total_tick%100)
+      boost();
+
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state != RUNNABLE)
+        continue;
+
+      // Switch to chosen process.  It is the process's job
+      // to release ptable.lock and then reacquire it
+      // before jumping back to us.
+      c->proc = p;
+      switchuvm(p);
+      p->state = RUNNING;
+
+      swtch(&(c->scheduler), p->context);
+      switchkvm();
+
+      // Process is done running for now.
+      // It should have changed its p->state before coming back.
+      c->proc = 0;
+    }
+    release(&ptable.lock);
+
+  }
+  
 }
 
 /*
