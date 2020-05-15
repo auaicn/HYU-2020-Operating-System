d66c80250e36be090ceff8e5c4524f9a1d1f9e03
2016024811 <crorowslowpp@gmail.com>
2020-05-15 21:44:27 +0900

    systhread

--- xv6-public/systhread.c
+++ xv6-public/systhread.c
@@ -4,37 +4,39 @@
 
 // thread.c
 int 
-sys_thread_create(thread_t *thread, void* (*start_rountine)(void*), void *arg)
+sys_thread_create(void)
 {
-  struct thread_t *thread_;
-  void* (*start_rountine)(void*)
-  void *arg
-  int major, minor;
+  thread_t *thread_;
+  void* (*start_rountine_)(void*);
+  void *arg_;
 
-  begin_op();
-  if((argstr(0, &path)) < 0 ||
-     argint(1, &major) < 0 ||
-     argint(2, &minor) < 0 ||
-     (ip = create(path, T_DEV, major, minor)) == 0){
-    end_op();
+  if(argint(0, &thread_) < 0          ||
+    argint(1, &start_rountine_) < 0   ||
+    argint(2, &arg_) < 0)
     return -1;
-  }
-  iunlockput(ip);
-  end_op();
-  return 0;
-	thread_t
+
+  return sys_thread_create(thread_t *thread_, void* (*start_rountine_)(void*), void *arg_);
 }
 
 void 
-sys_thread_exit(void *retval)
+sys_thread_exit(void)
 {
-	void* retval;
-	(argint(0,&retval))
+  void *retval_;
+  if(argint(0, &retval_) < 0)
+    return -1;
 
+  return sys_thread_exit(void *retval_);
 }
 
 int
 sys_thread_join(thread_t thread, void **retval)
 {
- 	
+ 	struct thread_t thread_;
+  void **retval_;
+
+  if(argint(0, &thread_) < 0
+    ||argint(1, &retval_) < 0)
+    return -1;
+
+  return sys_thread_join(thread_t thread_, void **retval_);
 }
