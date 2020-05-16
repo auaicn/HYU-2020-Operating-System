848008dbb1457ef8d0c3181a63721b5031eb9a9e
2016024811 <crorowslowpp@gmail.com>
2020-05-16 16:13:56 +0900

    my testcode

---
+++ project_2_test/thread_xv6.c
@@ -0,0 +1,72 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define NUM_THREAD (9)
+#define NUM_INCREASE (10000)
+
+int global_cnt = 0;
+
+/*
+struct thread_mutex_t{
+	// t means type
+	// implemented version : spinlock.
+	// this type is also implemented in 
+	struct spinlock lock;
+};
+*/
+
+struct thread_mutex_t my_lock;
+
+int ret_val[NUM_THREAD];
+
+void* increase(void* arg){
+	int* int_arg = (int*) arg;
+	printf("[%d Thread] pid[%d] %d\n",thread_self(), getpid(),int_arg[1]);
+
+	for (int i=0;i<int_arg[0];i++){
+		thread_mutex_lock(&my_lock);
+		global_cnt++;
+		thread_mutex_unlock(&my_lock);
+	}
+
+	thread_exit(&int_arg[1]);
+}
+
+void my_error(const char* str){
+	printf("[ERROR] %s\n",str);
+}
+
+int main(int argc, char const *argv[])
+{
+
+	thread_t pid[10];
+	thread_mutex_init(&my_lock,NULL);
+
+	printf("[%s Thread] pid[%d]\n","Master",getpid());
+
+	int arg[NUM_THREAD][2];
+	for (int i=0;i<NUM_THREAD;i++)
+		arg[i][0] = NUM_INCREASE;
+
+	for (int i = 0; i < NUM_THREAD; i++){
+		arg[i][1] = (i+1)*(i+1);
+		int ret = thread_create(&pid[i], NULL, &increase, (void*)arg[i]);
+		if(!ret)
+			continue;
+		else
+			my_error("thread_create");
+	}
+    int *ptr[NUM_THREAD];
+
+	void *ret_val[10];
+	for (int i=0;i<NUM_THREAD;i++){
+	    thread_join(pid[i], (void**)&(ptr[i]));
+
+	    printf("\n return value from first thread is [%d]\n", *ptr[i]);
+	}
+
+	printf("%d\n",global_cnt);
+
+	return 0;
+}
