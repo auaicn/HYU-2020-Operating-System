8a35a5e1b3668265b316f8ce92a34c8fe9972aab
2016024811 <crorowslowpp@gmail.com>
2020-06-20 05:05:08 +0900

    adjusted FSSIZE to be 40,000

--- project3/xv6-public/stress.c
+++ project3/xv6-public/stress.c
@@ -2,7 +2,7 @@
 #include "stat.h"
 #include "user.h"
 
-#define BIGSIZE (2097152)
+#define BIGSIZE (1000000)
 #define TIMES (100)
 
 char buf[BIGSIZE];
@@ -16,32 +16,33 @@ main(int argc, char *argv[])
   int n;
   int i;
 
-  printf(1,"Stress test for big file (size:2097152) Start\n");
+  char char_var = '1';
+ for (int i=0;i<BIGSIZE;i++){
+    if((i%512) == 0){
+      if(char_var == '9')
+        char_var = '0';
+      else
+        char_var ++;
+    }
+    buf[i] = char_var;
+  }
+
+  printf(1,"Stress test for big file (size:100,000,000) Start\n");
   for (i = 0;i < TIMES; i++){
     if((fd = open("stress.out",0x202)) < 0){
-        printf(1, "stress: cannot open %s\n", "test.out");
+        printf(1, "stress: cannot open %s\n", "stress.out");
         exit();
     }
 
-    char char_var = '1';
-    for (int i=0;i<BIGSIZE;i++){
-      if((i%512) == 0){
-        if(char_var == '9')
-          char_var = '0';
-        else
-          char_var ++;
-      }
-      buf[i] = char_var;
+    for (int i=0;i<100;i++){
+      if((n = write(fd,buf,BIGSIZE)) < 0)
+        printf(1,"[proc]writing failed\n");
     }
-
-    if((n = write(fd,buf,BIGSIZE)) < 0)
-      printf(1,"[proc]writing failed\n");
-
     close(fd);
     printf(1,"Test[%d] Finished\n",i+1);
     unlink("stress.out");
   }
-  printf(1,"Stress test for big file (size:2097152) Passed\n");
+  printf(1,"Stress test for big file (size:100,000,000) Passed\n");
 
   exit();  
 }
